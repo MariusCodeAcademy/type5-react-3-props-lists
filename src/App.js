000bb1f38ac1b35a7a90eeac3f7dae912a1161a9
Marius <marius@example.com>
2022-03-25 14:55:41 +0200

    heroSectionData

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import Header from './components/header/Header';
+import HeroSection from './components/heroSection/HeroSection';
+
+const navData = [
+  { link: '#home', title: 'Home' },
+  { link: '#about', title: 'About' },
+  { link: '#contact', title: 'Contact us' },
+];
+
+const heroSectionData = {
+  title: 'React is great',
+  subtitle: 'Not that hard to learn',
+  btnText: 'Learn more',
+};
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <h1>App</h1>
+      <HeroSection data={heroSectionData} />
     </div>
   );
 }
---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,8 @@
+// susikuriam tuscia komp HeroSection
+// isitraukiam i App.js
+
+function HeroSection() {
+  return <div>Hero section</div>;
+}
+
+export default HeroSection;
