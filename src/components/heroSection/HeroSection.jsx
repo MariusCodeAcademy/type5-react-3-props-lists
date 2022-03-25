b9211cb1c23aed3ad206d7ee45ada260a350925f
Marius <marius@example.com>
2022-03-25 14:59:56 +0200

    pass props hero

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,8 +1,16 @@
 // susikuriam tuscia komp HeroSection
 // isitraukiam i App.js
 
-function HeroSection() {
-  return <div>Hero section</div>;
+function HeroSection(props) {
+  return (
+    <div className='hero'>
+      <div className='info'>
+        <h1>{props.data.title}</h1>
+        <p className='hero__subtitle'>{props.data.subtitle}</p>
+        <button>{props.data.btnText}</button>
+      </div>
+    </div>
+  );
 }
 
 export default HeroSection;
